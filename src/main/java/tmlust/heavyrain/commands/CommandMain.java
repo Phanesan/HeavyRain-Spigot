9c0e3ba3eb8322247b5e8dba3827bcfa6610dcd2
TMLust <yahir.romo@hotmail.com>
2022-09-30 16:29:38 -0600

    added help command

--- src/main/java/tmlust/heavyrain/commands/CommandMain.java
+++ src/main/java/tmlust/heavyrain/commands/CommandMain.java
@@ -1,5 +1,59 @@
 package tmlust.heavyrain.commands;
 
-public class CommandMain {
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import tmlust.heavyrain.HeavyRain;
 
+import java.util.ArrayList;
+
+public class CommandMain implements CommandExecutor {
+
+    private final String commandLabel = HeavyRain.getPlugin().getCommandLabel();
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(args.length == 0){
+            printHelpCommands(sender);
+            return true;
+        } else {
+            switch(args[0].toLowerCase()){
+                case "startcountdown":
+
+                    return true;
+                default:
+                    printHelpCommands(sender);
+                    return true;
+            }
+        }
+    }
+
+    private void printHelpCommands(CommandSender sender){
+        if(sender instanceof Player){
+            Player player = (Player) sender;
+            for(String s : getListStringHelpCommands()){
+                player.sendMessage(s);
+            }
+            player.playSound(player, Sound.BLOCK_ANVIL_PLACE, 1, 2);
+        } else {
+            ConsoleCommandSender console = (ConsoleCommandSender) sender;
+            for(String s : getListStringHelpCommands()){
+                console.sendMessage(s);
+            }
+        }
+    }
+
+    private ArrayList<String> getListStringHelpCommands(){
+        ArrayList<String> stringHelpCommands = new ArrayList<String>();
+
+        stringHelpCommands.add(ChatColor.DARK_GREEN + "/" + commandLabel + ": " + ChatColor.GREEN + "Obten la lista de comandos.");
+        stringHelpCommands.add(ChatColor.DARK_GREEN + "/" + commandLabel + " StartCountdown: " + ChatColor.GREEN + "Inicia la cuenta atras de la Heavy Rain");
+
+        return stringHelpCommands;
+    }
 }
--- src/main/java/tmlust/heavyrain/files/Config.java
+++ src/main/java/tmlust/heavyrain/files/Config.java
@@ -1,26 +1,20 @@
 package tmlust.heavyrain.files;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import tmlust.heavyrain.HeavyRain;
-
-import java.io.File;
-import java.io.IOException;
 
 public class Config {
 
-    private static FileConfiguration fileConfig = HeavyRain.getInstance().getConfig();
+    private static final FileConfiguration fileConfig = HeavyRain.getPlugin().getConfig();
 
     public static void setup(){
 
         fileConfig.options().copyDefaults(true);
-        HeavyRain.getInstance().saveConfig();
+        HeavyRain.getPlugin().saveConfig();
 
     }
 
     public static void reload(){
-        HeavyRain.getInstance().saveConfig();
+        HeavyRain.getPlugin().saveConfig();
     }
 }
