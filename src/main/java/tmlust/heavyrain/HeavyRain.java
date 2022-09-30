9fdd6326d386aacaa504c40b2fdc801712b05ada
TMLustAlP <48665230+TMLustAlP@users.noreply.github.com>
2022-09-30 16:33:51 -0600

    Revert "added help command"

--- src/main/java/tmlust/heavyrain/HeavyRain.java
+++ src/main/java/tmlust/heavyrain/HeavyRain.java
@@ -1,38 +1,24 @@
 package tmlust.heavyrain;
 
-import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-import tmlust.heavyrain.commands.CommandMain;
 import tmlust.heavyrain.files.Config;
 
 public class HeavyRain extends JavaPlugin {
 
-	private static HeavyRain plugin;
-	private final String commandLabel = "heavyrain";
+	private static HeavyRain instance;
 
 	@Override
 	public void onEnable() {
-		plugin = this;
-
-		//Config setup
 		Config.setup();
-
-		//Commands setup
-		CommandsSetup();
 	}
 	
 	@Override
 	public void onDisable() {
 		
 	}
-
-	public static HeavyRain getPlugin() {
-		return plugin;
-	}
-	public String getCommandLabel() {
-		return commandLabel;
-	}
-	public void CommandsSetup(){
-		plugin.getCommand("heavyrain").setExecutor(new CommandMain());
+	
+	public static HeavyRain getInstance() {
+		return instance;
 	}
 }
--- src/main/java/tmlust/heavyrain/files/Config.java
+++ src/main/java/tmlust/heavyrain/files/Config.java
@@ -1,20 +1,26 @@
 package tmlust.heavyrain.files;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import tmlust.heavyrain.HeavyRain;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Config {
 
-    private static final FileConfiguration fileConfig = HeavyRain.getPlugin().getConfig();
+    private static FileConfiguration fileConfig = HeavyRain.getInstance().getConfig();
 
     public static void setup(){
 
         fileConfig.options().copyDefaults(true);
-        HeavyRain.getPlugin().saveConfig();
+        HeavyRain.getInstance().saveConfig();
 
     }
 
     public static void reload(){
-        HeavyRain.getPlugin().saveConfig();
+        HeavyRain.getInstance().saveConfig();
     }
 }
